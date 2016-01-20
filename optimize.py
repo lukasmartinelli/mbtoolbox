adfc1d6ad9a1f009cced90a6254ffb32d19841a2
Lukas Martinelli <me@lukasmartinelli.ch>
2016-01-20 13:51:25 +0100

    Support removing tiles from MBTiles

--- optimize.py
+++ optimize.py
@@ -2,6 +2,7 @@
 """Remove descendant tiles below a parent tile with specified mask level.
 Usage:
   optimize.py check <mbtiles_file> -z=<mask_level> [--scheme=<scheme>]
+  optimize.py optimize <mbtiles_file> -z=<mask_level> [--scheme=<scheme>]
   optimize.py (-h | --help)
   optimize.py --version
 
@@ -26,6 +27,15 @@ class MBTiles:
     def __init__(self, mbtiles_file, scheme):
         self.conn = mbutil.mbtiles_connect(mbtiles_file)
         self.scheme = scheme
+        self._tiles_view = self.using_tiles_view()
+
+    def using_tiles_view(self):
+        rs = self.conn.execute("""
+            SELECT COUNT(*) FROM sqlite_master
+            WHERE (type='view' AND name='tiles')
+               OR (type='table' AND name='map') COLLATE NOCASE
+        """).fetchone()
+        return rs[0] == 2
 
     def tiles_at_zoom_level(self, z):
         query = (
@@ -41,14 +51,36 @@ class MBTiles:
                 y = mbutil.flip_y(z, y)
             yield mercantile.Tile(x, y, z)
 
+    def remove_tiles(self, tiles):
+        for tile in tiles:
+            self.remove_tile(x=tile.x, y=tile.y, z=tile.z)
+        self.conn.commit()
+
+    def remove_tile(self, x, y, z):
+        if self.scheme == 'tms':
+            y = mbutil.flip_y(z, y)
+
+        if self._tiles_view:
+            delete_query = (
+                'delete from map where zoom_level={} ' +
+                'and tile_column={} and tile_row={}'
+            ).format(z, x, y)
+        else:
+            delete_query = (
+                'delete from tiles where zoom_level={} ' +
+                'and tile_column={} and tile_row={}'
+            ).format(z, x, y)
+
+        self.conn.execute(delete_query)
+
     def inspect_tile(self, x, y, z):
         if self.scheme == 'tms':
             y = mbutil.flip_y(z, y)
 
         query = (
-            'select tile_data from tiles where zoom_level={} and tile_column={} and tile_row={}'
-            .format(z, x, y)
-        )
+            'select tile_data from tiles where zoom_level={} ' +
+            'and tile_column={} and tile_row={}'
+        ).format(z, x, y)
         rs = self.conn.execute(query).fetchone()
         if rs:
             raw = rs[0]
@@ -67,8 +99,7 @@ def all_descendant_tiles(x, y, zoom, max_zoom):
                 yield desc_tile
 
 
-def find_optimizable_tiles(mbtiles_file, maskLevel, scheme):
-    mbtiles = MBTiles(mbtiles_file, scheme)
+def find_optimizable_tiles(mbtiles, maskLevel, scheme):
     parent_tiles = [t for t in mbtiles.tiles_at_zoom_level(maskLevel)]
 
     def descendant_checksums(x, y, zoom, max_zoom):
@@ -84,8 +115,21 @@ def find_optimizable_tiles(mbtiles_file, maskLevel, scheme):
             yield tile
 
 
+def remove_subpyramids(mbtiles_file, maskLevel, scheme):
+    mbtiles = MBTiles(mbtiles_file, scheme)
+
+    for tile in find_optimizable_tiles(mbtiles, maskLevel, scheme):
+        desc_tiles = list(all_descendant_tiles(x=tile.x, y=tile.y,
+                                               zoom=tile.z, max_zoom=14))
+        mbtiles.remove_tiles(desc_tiles)
+        for desc_tile in desc_tiles:
+            print('{}/{}/{}\t{}'.format(desc_tile.z, desc_tile.x,
+                                        desc_tile.y, 'REMOVED'))
+
+
 def check_masked_tiles(mbtiles_file, maskLevel, scheme):
-    tiles = list(find_optimizable_tiles(mbtiles_file, maskLevel, scheme))
+    mbtiles = MBTiles(mbtiles_file, scheme)
+    tiles = list(find_optimizable_tiles(mbtiles, maskLevel, scheme))
     for tile in tiles:
         print('{}/{}/{}\t{}'.format(tile.z, tile.x, tile.y, 'OPTIMIZABLE'))
     return len(tiles)
@@ -99,3 +143,9 @@ if __name__ == '__main__':
             int(args['-z']),
             args['--scheme']
         ))
+    if args.get('optimize'):
+        remove_subpyramids(
+            args['<mbtiles_file>'],
+            int(args['-z']),
+            args['--scheme']
+        )
